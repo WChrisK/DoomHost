29d7e1c9b55e07aa589ae3cb42e0717937e2d044
Chris <38708476+wchrisk@users.noreply.github.com>
2019-02-27 01:08:15 -0500

    Added spring boot basics

---
+++ backend/src/main/java/ca/doomhost/RootController.java
@@ -0,0 +1,13 @@
+package ca.doomhost;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class RootController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
